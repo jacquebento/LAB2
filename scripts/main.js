51d6a1da6816425d96dccaacdc2bdfa02ba6bcae
Jacqueline Bento <104387400+jacquebento@users.noreply.github.com>
2023-11-28 21:13:22 -0700

    Java Script Lab

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,7 @@ const fullName = (first, drink) => {
     console.log(first + "'s favorite drink is " + drink)
 }
 
-fullName("Jacqueline", "milk");
+fullName("Jacqueline", "Coke zero with lemon");
 
 // show the year in the footer
 let date =  new Date().getFullYear();
@@ -24,57 +24,42 @@ document.querySelector("#date").innerHTML = date;
 
 // button
 document.getElementById('btn-alert').onmouseover = () => {
-    document.getElementById('btn-alert').innerText = 'new button label';
+    document.getElementById('btn-alert').innerText = 'New button label';
 };
 
 document.getElementById('btn-alert').onmouseleave = () => {
-    document.getElementById('btn-alert').innerText = 'default button label';
+    document.getElementById('btn-alert').innerText = 'Default button label';
 };
 
-// counter button and text colour change
-let count = 1;
-document.getElementById('btn-counter').onclick = () => {
+// counter button, text colour change and odd or even check
+    let count = 1;
+     document.getElementById('btn-counter').onclick = () => {
      count = count + 1;
-     document.getElementById('txt-counter').innerHTML = 'Number: ' + count
-    //  classList = [];
-     if(count % 2 == 0){
-        document.getElementById('txt-counter').style.color = "blue";
-        document.getElementById('txt-counter').classList.add('even');
-        console.log("Class List: " + document.getElementById('txt-counter').classList)
-        } else {
-        document.getElementById('txt-counter').style.color = "red";
-        document.getElementById('txt-counter').classList.add('odd');
-        console.log("Class List: " + document.getElementById('txt-counter').classList)
-        }  
+     document.getElementById('txt-counter').style.color = count % 2 === 0 ? 'blue' : 'red';
+     document.getElementById('txt-counter').classList = count % 2 === 0 ? 'even' : 'odd';
+    
+    document.getElementById('txt-counter').innerHTML = 'Number: ' + count + ' - ' + document.getElementById('txt-counter').classList;
 }
 
-//for loop
-// Get the ordered list element by its id
+
+// for loop to create a list - you can see the list in the console
+console.log('');
+console.log('This is the ordered list that was requested:');
+
+for (let i = 1; i <= 100; i++) {
+// ordered list element by its id
 const numbersList = document.getElementById('numbers');
 
-// Loop to populate the ordered list with 100 child elements
-for (let i = 1; i <= 100; i++) {
-  // Create a new <li> element with the createElement() document method
-  const listItem = document.createElement('li');
+// Create a new <li> element with the createElement() document method
+    const listItem = document.createElement('li');
 
-  // Set the text content of the new <li> element based on even or odd index
-  listItem.textContent = i % 2 === 0 ? 'even' : 'odd';
-
-  // Append the <li> element to the ordered list
-  numbersList.appendChild(listItem);
+// Set the text content of the new <li> element based on even or odd index
+    listItem.textContent = i % 2 === 0 ? 'even' : 'odd';
+    
+    console.log(i + ' ' +  listItem.textContent)
+// Append the <li> element to the ordered list
+    numbersList.appendChild(listItem);
 }
 
-
-
-
-const getOdd = (count) => {
-    remainder = count % 2
-    console.log("Count:" + remainder)
-    if (remainder = 0) {
-    document.getElementById('id').classList.add('even');
-    } else {
-    document.getElementById('id').classList.add('odd');
-    }
-}
 
 
