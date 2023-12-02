708e038d3dd6cf3d583091552ee67e840fc0b791
Jacqueline Bento <104387400+jacquebento@users.noreply.github.com>
2023-12-01 19:48:23 -0700

    update buttons and list

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,35 @@
-console.log('Hello, World!');
-// this is an alert
-// alert('Hello, World!');
 
-const FIRSTNAME = 'Jacqueline';
-// const do not change, you can't assign a new value to a constant
-let lastName = 'Bento';
-lastName = 'Bento';
-// var lastName = "Caetano"; // ES5 - older sintax
-let age = 25;
-age = true;
-// console.log(typeof age, age)
-// console.log(FIRSTNAME, lastName);
-
-const fullName = (first, drink) => {
-    console.log(first + "'s favorite drink is " + drink)
-}
-
-fullName("Jacqueline", "Coke zero with lemon");
 
 // show the year in the footer
 let date =  new Date().getFullYear();
 document.querySelector("#date").innerHTML = date;
 
-// button
-document.getElementById('btn-alert').onmouseover = () => {
-    document.getElementById('btn-alert').innerText = 'New button label';
+// alert button and hover button
+document.getElementById('btn-alert').onclick = () => {
+    alert('Hello, World!');
+}
+
+    document.getElementById('btn-alert').onmouseover = () => {
+    document.getElementById('btn-alert').innerText = 'Click here';
 };
 
-document.getElementById('btn-alert').onmouseleave = () => {
-    document.getElementById('btn-alert').innerText = 'Default button label';
+    document.getElementById('btn-alert').onmouseleave = () => {
+    document.getElementById('btn-alert').innerText = 'Alert Button';
 };
 
 // counter button, text colour change and odd or even check
     let count = 1;
      document.getElementById('btn-counter').onclick = () => {
      count = count + 1;
+     console.log(count)
      document.getElementById('txt-counter').style.color = count % 2 === 0 ? 'blue' : 'red';
      document.getElementById('txt-counter').classList = count % 2 === 0 ? 'even' : 'odd';
-    
+    console.log(document.getElementById('txt-counter').classList)
     document.getElementById('txt-counter').innerHTML = 'Number: ' + count + ' - ' + document.getElementById('txt-counter').classList;
 }
 
 
-// for loop to create a list - you can see the list in the console
-console.log('');
-console.log('This is the ordered list that was requested:');
+// for loop to create a list
 
 for (let i = 1; i <= 100; i++) {
 // ordered list element by its id
@@ -55,11 +40,10 @@ const numbersList = document.getElementById('numbers');
 
 // Set the text content of the new <li> element based on even or odd index
     listItem.textContent = i % 2 === 0 ? 'even' : 'odd';
+    listItem.style.backgroundColor = i % 2 === 0 ? 'mediumblue' : 'lightblue';
     
     console.log(i + ' ' +  listItem.textContent)
 // Append the <li> element to the ordered list
     numbersList.appendChild(listItem);
 }
-
-
 
